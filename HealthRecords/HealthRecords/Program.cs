4efc287bd777b0b0d531e659d2f129484ddaac34
Asumakn <143426903+Asumakn@users.noreply.github.com>
2024-09-04 21:20:22 -0400

    added files for assignement 1
    Heath records
    Health Rates

---
+++ HealthRecords/HealthRecords/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthRecords
+{
+    internal class Program
+    {
+        private String firstName;
+        private String lastName;
+        private int birthYear;
+        private int height;
+        private int weight;
+        private int currentYear;
+
+        public Program(string firstName, string lastName, int birthYear, int height, int weight, int currentYear)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthYear = birthYear;
+            this.Height = height;
+            this.Weight = weight;
+            this.CurrentYear = currentYear;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int BirthYear { get => birthYear; set => birthYear = value; }
+        public int Height { get => height; set => height = value; }
+        public int Weight { get => weight; set => weight = value; }
+        public int CurrentYear { get => currentYear; set => currentYear = value; }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public int calculateAge( int currentYear, int birthYear)
+        {
+return currentYear - birthYear;
+        }
+        public int maxHeartRate(int age)
+        {
+            return 220 - age;
+        }
+
+        public int targetMaxHeartRate(int age)
+        {
+            return (220 - age) * (17 / 20);
+        }
+        public int targetMinHeartRate(int age)
+        {
+            return (220 - age) * (5 / 10);
+        }
+public int calculateBMI(int weight)
+        {
+            return (weight * 703) / height ^ 2;
+        }
+
+    }
+}
---
+++ HeartRates/HeartRates/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartRates
+{
+    internal class Program
+    {
+
+        private String firstName;
+        private String lastName;
+        private int birthYear;
+        private int currentYear;
+
+        public String getFirstName() {  return firstName; }
+        public String getLastName() { return lastName; }
+        public int getbirthYear() { return birthYear;}
+        public int getCurrentYear() { return currentYear; }
+
+        public void setFirstName(String newString) {  firstName = newString; }
+        public void setLastName(String newString) { lastName = newString; }
+        public void setbirthYear(int newInt) { birthYear = newInt; }
+        public void setCurrentYear(int newInt) { currentYear = newInt; }
+
+        public Program(string firstName, string lastName, int birthYear, int currentYear)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthYear = birthYear;
+            this.currentYear = currentYear;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public int calculateAge(int birthYear, int currentYear)
+        {
+            return currentYear - birthYear;
+        }
+        public int maxHeartRate(int age)
+        {
+            return 220 - age;
+        }
+
+        public int targetMaxHeartRate(int age)
+        {
+            return (220 - age) *(17/20);
+        }
+        public int targetMinHeartRate(int age)
+        {
+            return (220 - age) * (5 / 10);
+        }
+
+
+    }
+
+
+}
+
